b05a52db98de9d8885aed49339e583bff61185f8
Rawley Fowler <rawleyfowler@proton.me>
2023-11-30 22:49:17 -0600

    make it do the do

--- app.pl
+++ app.pl
@@ -8,12 +8,12 @@ use constant SNAKE_NAME => 'perlsnake';
 
 set serializer => 'JSON';
 
-sub _optimal_movements {
+sub _optimal_movement {
     my ( $viable_movements, $food, $hazards ) = @_;
 
     foreach my $m (@$viable_movements) {
         my @dt =
-          map { abs( $_->{x} - $m->{x} ) + abs( $_->{y} - $m->{y} ) } @$food;
+            map { abs( $_->{x} - $m->{x} ) + abs( $_->{y} - $m->{y} ) } @$food;
 
         # TODO: Add based on hazards.
 
@@ -28,8 +28,13 @@ sub _viable_movements {
 
     my @movements;
 
-    my @hx = map { $_->{x} } @$hazards;
-    my @hy = map { $_->{y} } @$hazards;
+    my %hx;
+    my %hy;
+
+    for (@$hazards) {
+        $hx{$_->{x}} = 1;
+        $hy{$_->{y}} = 1;
+    }
 
     my $dw = $width - 1;
     my $dh = $height - 1;
@@ -42,25 +47,40 @@ sub _viable_movements {
         return +{ direction => $dir, x => $nx, y => $ny };
     };
 
+    my $safe = sub {
+        my ($nx, $ny) = @_;
+
+        return not(exists $hx{$nx} && exists $hy{$ny}) &&
+            ($nx >= 0 && $nx < $dw) &&
+            ($ny >= 0 && $ny < $dh);
+    };
+
+    say 'HAZARDS';
+    p %hx;
+    p %hy;
+
     if ( ( my $dx = $x + 1 ) < $dw ) {
         push @movements, $make_direction->( 'right', $dx, $y )
-          unless grep { $_ == $dx } @hx;
+            if $safe->($dx, $y);
     }
 
     if ( ( my $dx = $x - 1 ) >= 0 ) {
         push @movements, $make_direction->( 'left', $dx, $y )
-          unless grep { $_ == $dx } @hx;
+            if $safe->($dx, $y);
     }
 
     if ( ( my $dy = $y + 1 ) < $dh ) {
         push @movements, $make_direction->( 'up', $x, $dy )
-          unless grep { $_ == $dy } @hy;
+            if $safe->($x, $dy);
     }
 
-    if ( ( my $dy = $y - 1 ) > 0 ) {
+    if ( ( my $dy = $y - 1 ) >= 0 ) {
         push @movements, $make_direction->( 'down', $x, $dy )
-          unless grep { $_ == $dy } @hy;
+            if $safe->($x, $dy);
     }
+
+    say 'MOVEMENTS';
+    p @movements;
 
     return \@movements;
 }
@@ -74,42 +94,38 @@ sub _determine_move {
     my $snake = $map->{you};
 
     my $head    = $snake->{head};
+    say 'HEAD';
+    p $head;
     my $tail    = pop $snake->{body}->@*;
     my $height  = $board->{height};
     my $width   = $board->{width};
     my $hazards = $board->{hazards};
     my $food    = $board->{food};
 
-    $snake->{body} = [ grep { $_->{x} != $head->{x} && $_->{y} != $head->{y} }
-          $snake->{body}->@* ];
+    p $snake;
 
     push @$hazards, $snake->{body}->@*;
 
-    goto SOLO unless $game->{snakes};
+    unless ($game->{snakes}) {
+        for ( $game->{snakes}->@* ) {
+            next if $_->{id} eq $snake->{id};
 
-    for ( $game->{snakes}->@* ) {
-        next if $_->{id} eq $snake->{id};
-
-        # Hazards includes bigger snake bodies
-        if ( $_->{health} > $snake->{health} ) {
-            push @$hazards, $_->{body}->@*;
-        }
-        else {
-            my @body =
-              grep { $_->{x} != $_->{head}->{x} && $_->{y} != $_->{head}->{y} }
-              $_->{body}->@*;
-            push @$hazards, @body;
+            # Hazards includes bigger snake bodies
+            if ( $_->{health} > $snake->{health} ) {
+                push @$hazards, $_->{body}->@*;
+            } else {
+                my @body =
+                    grep { $_->{x} != $_->{head}->{x} && $_->{y} != $_->{head}->{y} }
+                    $_->{body}->@*;
+                push @$hazards, @body;
+            }
         }
     }
 
-  SOLO:
-
     my $viable_movements =
-      _viable_movements( $head, $hazards, $width, $height );
+        _viable_movements( $head, $hazards, $width, $height );
     my $optimal_movement =
-      _optimal_movement( $viable_movements, $food, $hazards );
-
-    p $optimal_movement;
+        _optimal_movement( $viable_movements, $food, $hazards );
 
     return +{
         shout => 'PERL MAY BE OLD, BUT SHE SURE IS FAST!',
@@ -134,8 +150,9 @@ post '/start' => sub {
 
 post '/move' => sub {
     my $move = from_json( request->body );
+    $move = _determine_move($move);
     p $move;
-    return _determine_move($move);
+    return $move;
 };
 
 return to_app if $ENV{SNAKE_PRODUCTION};
