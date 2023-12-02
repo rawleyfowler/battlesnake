bd1f28448e7a049096a8b4a9606ffb14d00accdb
Rawley <rawleyfowler@proton.me>
2023-12-02 14:56:47 -0600

    Change color, format

--- app.pl
+++ app.pl
@@ -13,7 +13,7 @@ sub _optimal_movement {
 
     foreach my $m (@$viable_movements) {
         my @dt =
-            map { abs( $_->{x} - $m->{x} ) + abs( $_->{y} - $m->{y} ) } @$food;
+          map { abs( $_->{x} - $m->{x} ) + abs( $_->{y} - $m->{y} ) } @$food;
 
         # TODO: Add based on hazards.
 
@@ -31,7 +31,7 @@ sub _viable_movements {
     my %h;
 
     for (@$hazards) {
-        $h{$_->{x} . ',' . $_->{y}} = 1;
+        $h{ $_->{x} . ',' . $_->{y} } = 1;
     }
 
     my $dw = $width - 1;
@@ -46,11 +46,12 @@ sub _viable_movements {
     };
 
     my $safe = sub {
-        my ($nx, $ny) = @_;
+        my ( $nx, $ny ) = @_;
 
-        return not(exists $h{$nx . ',' . $ny}) &&
-            ($nx >= 0 && $nx < $dw) &&
-            ($ny >= 0 && $ny < $dh);
+        return
+             not( exists $h{ $nx . ',' . $ny } )
+          && ( $nx >= 0 && $nx < $dw )
+          && ( $ny >= 0 && $ny < $dh );
     };
 
     say 'HAZARDS';
@@ -59,25 +60,25 @@ sub _viable_movements {
     if ( ( my $dx = $x + 1 ) < $dw ) {
         say 'DX: ', $dx, ' Y: ', $y;
         push @movements, $make_direction->( 'right', $dx, $y )
-            if $safe->($dx, $y);
+          if $safe->( $dx, $y );
     }
 
     if ( ( my $dx = $x - 1 ) >= 0 ) {
         say 'DX: ', $dx, ' Y: ' . $y;
         push @movements, $make_direction->( 'left', $dx, $y )
-            if $safe->($dx, $y);
+          if $safe->( $dx, $y );
     }
 
     if ( ( my $dy = $y + 1 ) < $dh ) {
         say 'X: ', $x, ' DY: ', $dy;
         push @movements, $make_direction->( 'up', $x, $dy )
-            if $safe->($x, $dy);
+          if $safe->( $x, $dy );
     }
 
     if ( ( my $dy = $y - 1 ) >= 0 ) {
         say 'X: ', $x, ' DY: ', $dy;
         push @movements, $make_direction->( 'down', $x, $dy )
-            if $safe->($x, $dy);
+          if $safe->( $x, $dy );
     }
 
     say 'MOVEMENTS';
@@ -94,7 +95,7 @@ sub _determine_move {
     my $board = $map->{board};
     my $snake = $map->{you};
 
-    my $head    = $snake->{head};
+    my $head = $snake->{head};
     say 'HEAD';
     p $head;
     my $tail    = pop $snake->{body}->@*;
@@ -108,26 +109,28 @@ sub _determine_move {
     shift $snake->{body}->@*;
     push @$hazards, $snake->{body}->@*;
 
-    unless ($game->{snakes}) {
+    unless ( $game->{snakes} ) {
         for ( $game->{snakes}->@* ) {
             next if $_->{id} eq $snake->{id};
 
             # Hazards includes bigger snake bodies
             if ( $_->{health} > $snake->{health} ) {
                 push @$hazards, $_->{body}->@*;
-            } else {
+            }
+            else {
                 my @body =
-                    grep { $_->{x} != $_->{head}->{x} && $_->{y} != $_->{head}->{y} }
-                    $_->{body}->@*;
+                  grep {
+                    $_->{x} != $_->{head}->{x} && $_->{y} != $_->{head}->{y}
+                  } $_->{body}->@*;
                 push @$hazards, @body;
             }
         }
     }
 
     my $viable_movements =
-        _viable_movements( $head, $hazards, $width, $height );
+      _viable_movements( $head, $hazards, $width, $height );
     my $optimal_movement =
-        _optimal_movement( $viable_movements, $food, $hazards );
+      _optimal_movement( $viable_movements, $food, $hazards );
 
     return +{
         shout => 'PERL MAY BE OLD, BUT SHE SURE IS FAST!',
@@ -139,7 +142,7 @@ get '/' => sub {
     return +{
         apiversion => '1',
         author     => 'rawleyfowler',
-        color      => '#ffffff',
+        color      => '#0f0faa',
         head       => 'default',
         tail       => 'default'
     };
